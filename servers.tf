7885ab9bfa94d0edab0e691d1819bb7e7247826c
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-04-24 18:40:31 +0530

    Terraform Instances

--- servers.tf
+++ servers.tf
@@ -1,4 +1,16 @@
-resource "aws_instance" "frontend" {
+data "aws_ami" "centos" {
+  owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "centos-8-DevOps-Practice"
+
+}
+
+output "ami" {
+  value = data.aws_ami.centos.image_id
+}
+
+
+  resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
