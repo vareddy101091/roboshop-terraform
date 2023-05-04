369c1ad566aa3ccf0fa80190e355ab30b799caaa
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-04 18:10:26 +0530

    Terraform Instances

--- servers.tf
+++ servers.tf
@@ -8,6 +8,22 @@ resource "aws_instance" "instance" {
   tags = {
     Name = each.value["name"]
   }
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/vardevops/roboshop-shell",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}.sh"
+    ]
+  }
 }
 
 
