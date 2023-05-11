613401ebef6762294f19922099cca01e48939177
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-11 16:58:59 +0530

    Ansible

--- module/locals.tf
+++ module/locals.tf
@@ -7,7 +7,8 @@ locals {
      "sudo bash ${var.component_name}.sh ${var.password}"
     ]
   app_commands = [
-    "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/vareddy101091/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+  //  "sudo labauto ansible",
+    // "ansible-pull -i localhost, -U https://github.com/vareddy101091/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+    "echo OK"
   ]
 }
