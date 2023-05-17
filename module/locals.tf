7b8cfdc12d44e9154ca7ed7bcd0544bacf792c18
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-18 03:33:34 +0530

    Ansible

--- module/locals.tf
+++ module/locals.tf
@@ -11,4 +11,11 @@ locals {
     "ansible-pull -i localhost, -U https://github.com/vareddy101091/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
 
   ]
+  db_tags = {
+    Name = "${var.component_name}-${var.env}"
+  }
+  app_tags = {
+    Name    = "${var.component_name}-${var.env}"
+    Monitor = "true"
+  }
 }
