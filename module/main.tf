9e903a6a413f1075a484c1abbe5b67c4d848f9ef
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-18 06:05:15 +0530

    Ansible

--- module/main.tf
+++ module/main.tf
@@ -20,9 +20,8 @@ resource "null_resource" "provisioner" {
       host     = aws_instance.instance.private_ip
     }
 
-    inline =[
-     var.app_type == "db" ? local.db_commands : local.app_commands
-    ]
+    inline = var.app_type == "db" ? local.db_commands : local.app_commands
+
   }
 }
 
