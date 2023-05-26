40842e56073c13a927a459abdbe525890112b4f0
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-26 16:33:40 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source = "https://github.com/vareddy101091/tf-module-vpc.git"
+  source = "git::https://github.com/vareddy101091/tf-module-vpc.git"
 
-  for_each = var.vpc
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
 }
