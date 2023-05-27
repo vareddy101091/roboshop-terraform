0132dcfa31f8209eab869da9d16ee985b3d3547e
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-27 19:13:10 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ module "vpc" {
   env = var.env
 }
 
-module "web" {
+/*module "web" {
   source = "git::https://github.com/vareddy101091/tf-module-app.git"
 
   for_each = var.app
   instance_type = each.value["instance_type"]
   subnet_id = element(lookup(lookup(lookup(module.vpc, "main", null),each.value["subnet_name"], null), "subnet_ids", null), 0)
-}
+}*/
 
 //element(lookup(lookup(module.vpc, each.value["subnet_name"], null), "subnet_ids", null), 0)
